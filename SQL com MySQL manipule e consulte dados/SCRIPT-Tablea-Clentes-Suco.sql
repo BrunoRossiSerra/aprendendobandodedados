650abefe1f7b29b7b31d18b83ff10e386deeeafd
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-11 19:06:25 -0300

    TABELA sUCO
    primeira tabela criada

---
+++ SQL com MySQL manipule e consulte dados/SCRIPT-Tablea-Clentes-Suco.sql
@@ -0,0 +1,17 @@
+create database suco;
+use suco;
+create table tbCliente(
+CPF varchar(11),
+NOME varchar(100),
+ENDERECO1 varchar(150),
+ENDERECO2 varchar(150),
+BAIRRO varchar(50),
+CIDADE varchar(50),
+ESTADO varchar (50),
+CEP varchar(8),
+IDADE smallint,
+SEXO varchar(1),
+LIMITE_CREDITO float,
+VOLUME_COMPRA float,
+PRIMEIRA_COMPRA bit(1)
+);
