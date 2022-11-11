a07fa05688e2865c57c21b6e63343355ef7f7867
BrunoRossiSerra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-11 17:53:30 -0300

    Aula 1
    Primeiros comandos e criação de banco de dados

---
+++ aula-01.sql
@@ -0,0 +1,13 @@
+CREATE database cadastro;
+use cadastro;
+create table pessoaso(
+nome varchar (30),
+idade tinyblob, 
+sexo char(1), 
+peso float,
+altura float, 
+nacionalidade varchar (20)
+
+);
+
+describe pessoaso;
