05ebb8b5eb01c434a8262ac5e76ec354a08f9c84
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-12 13:56:51 -0300

    deletando vendedor

--- SQL com MySQL manipule e consulte dados/alterar-dados-na-planilha.sql
+++ SQL com MySQL manipule e consulte dados/alterar-dados-na-planilha.sql
@@ -4,7 +4,6 @@ update tbprodutos set embalagem ='lata',
 preco_lista = 2.45
 where produto = '544931';
 
-update tbprodutos set embalagem ='garrafa', preco_lista =5.55
+update tbprodutos set nome ='doce inverno',embalagem ='garrafa', preco_lista =5.55
 where produto = '1078680';
 
-select * from tbprodutos;
---
+++ SQL com MySQL manipule e consulte dados/apagar-produtos.sql
@@ -0,0 +1,5 @@
+select * from tb_vendedores;
+
+delete from tbprodutos where produto ='1087597';
+
+delete from tb_vendedores  where matricula ='0240';
