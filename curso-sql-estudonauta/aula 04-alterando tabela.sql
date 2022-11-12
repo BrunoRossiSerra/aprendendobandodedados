fbf53d925f70dcf2afa24caf28060258baf0c425
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-12 19:21:56 -0300

    comandos alter table
    comandos de alter table e suas variações e possibilidades

---
+++ curso-sql-estudonauta/aula 04-alterando tabela.sql
@@ -0,0 +1,37 @@
+use cadastro;
+
+describe pessoas;
+alter table pessoas 
+add column profissao varchar (10)
+after nome;
+
+alter table pessoas
+add codigo int first;
+
+alter table pessoas 
+drop column profissao; 
+
+alter table pessoas
+change profissao prof varchar(27);
+
+alter table pessoas
+rename to cadastro_pessoas;
+
+create table if not exists cursos(
+nome varchar (30) not null unique, 
+descricao text,
+carga int unsigned,
+total_aula int unsigned,
+ano year default '2020'
+)default charset = utf8mb3;
+
+alter table cursos
+add column idcurso int first;
+
+alter table cursos
+add primary key (idcurso);
+
+drop table if exists cursos;
+
+
+desc cursos;
