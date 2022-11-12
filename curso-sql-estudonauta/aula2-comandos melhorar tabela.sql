c6ce96124c3681db16c08489c15500877bfff675
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-12 10:55:07 -0300

    criaçãa da tabela pessoa
    aprimorando a tabela e otimizando os campos

--- curso-sql-estudonauta/aula2-comandos melhorar tabela.sql
+++ curso-sql-estudonauta/aula2-comandos melhorar tabela.sql
@@ -1,4 +1,16 @@
-create database cadastro1
+create database cadastro
 default character set utf8
 default collate utf8_general_ci;
 
+create table pessoas (
+id int not null auto_increment,
+nome varchar (30) not null,
+idade  date,
+sexo enum ('M', 'F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar (20) default 'Brasil',
+primary key(id)
+)default character set utf8;
+
+use cadastro;
