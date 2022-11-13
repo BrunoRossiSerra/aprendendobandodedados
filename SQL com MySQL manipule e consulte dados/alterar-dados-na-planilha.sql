2d017c4b1881e26617d44b795a0918057955783a
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-13 11:51:29 -0300

    filtrar dados
    buscando dados

--- SQL com MySQL manipule e consulte dados/alterar-dados-na-planilha.sql
+++ SQL com MySQL manipule e consulte dados/alterar-dados-na-planilha.sql
@@ -4,6 +4,7 @@ update tbprodutos set embalagem ='lata',
 preco_lista = 2.45
 where produto = '544931';
 
-update tbprodutos set nome ='doce inverno',embalagem ='garrafa', preco_lista =5.55
+update tbprodutos set nome ='doce vento', embalagem ='garrafa', preco_lista =5.55
 where produto = '1078680';
 
+select * from tbprodutos;
---
+++ SQL com MySQL manipule e consulte dados/comandos visualizar tabela e informações.sql
@@ -0,0 +1,11 @@
+select * from tb_vendedores; 
+
+select cpf, nome from tbcliente;
+select PRODUTO, SABOR,PRECO_LISTA from tbproduto limit 3;
+
+select produto, embalagem, sabor, PRECO_LISTA from tbproduto;
+
+
+select cpf as cpf_cliente, nome as nome_cliente from tbcliente;
+
+select nome, matricula from tb_vendedores;
