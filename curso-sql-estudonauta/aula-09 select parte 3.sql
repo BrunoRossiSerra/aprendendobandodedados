9c9e1e114f7a35000aeadf210c80c1a68e47b6a4
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-23 19:06:44 -0300

    select 3
    having
    group by

--- curso-sql-estudonauta/aula-09 select parte 3.sql
+++ curso-sql-estudonauta/aula-09 select parte 3.sql
@@ -5,3 +5,43 @@ select totaulas, count(*) from cursos
 group by totaulas
 order by totaulas;
 
+select carga, count(*) from cursos 
+where totaulas = 30
+group by carga
+having count(nome) > 3;
+
+select ano, count(*) from cursos
+group by ano
+having count(ano) >= 5
+order by count(*);
+
+select avg (carga) from curos;
+
+select carga, count(*) from cursos
+where ano > 2015
+group by carga
+having carga >(select avg (carga) from cursos)
+;
+
+select profissao,count(profissao) from gafanhotos
+group by profissao
+having count(*) <5;
+
+select sexo, count(*) from gafanhotos
+where  nascimento >'2005-01-01'
+group by sexo;
+
+select nacionalidade, count(nome) from gafanhotos
+where nacionalidade <> 'Brasil'
+group by nacionalidade
+having count(nome) > 3
+order by count(*);
+
+select avg(altura) from gafanhotos;
+
+select  peso, altura, count(nome) from gafanhotos
+where peso > 100
+group by altura
+having altura >(select avg(altura) from gafanhotos);
+
+
