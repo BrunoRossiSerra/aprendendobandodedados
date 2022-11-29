766141d241b4bf9f6ef56343fef19d220950ee01
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-11-28 21:18:56 -0300

    join

---
+++ curso-sql-estudonauta/chave estrangeira.sql
@@ -0,0 +1,25 @@
+use cadastro;
+describe gafanhotos;
+alter table gafanhotos
+add column cursopreferido int;
+
+desc cursos;
+alter table gafanhotos
+add foreign key (cursopreferido)
+references cursos(idcurso);
+
+select * from gafanhotos;
+select * from cursos;
+update gafanhotos set cursopreferido ='6'
+where id ='1';
+
+delete from cursos
+where idcurso ='6';
+
+
+select gafanhotos.nome, gafanhotos.cursopreferido,
+cursos.nome, cursos.ano
+from gafanhotos inner join cursos
+on cursos.idcurso = gafanhotos.cursopreferido
+order by cursos.nome;
+
